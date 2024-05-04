49943c39da670a3cc222c0a7db0927dd9dca6b4c
Ayan Mazumdar <164352702+ayan-mazumdar@users.noreply.github.com>
2024-05-04 21:14:09 +0530

    maths and numbers

---
+++ )7_numbers_and_math.js
@@ -0,0 +1,27 @@
+// const balance = new Number(100)
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber=23.5634
+
+// console.log(otherNumber.toPrecision(3));
+
+
+
+// +++++++++++++maths+++++++++++++++
+
+console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4,6,54.7));
+// console.log(Math.max(4,6,54.7));
+
+
+console.log((Math.random()*10)+1);
+const min= 10
+const max = 20 
+
+console.log(Math.floor(Math.random()*(max-min +1))
