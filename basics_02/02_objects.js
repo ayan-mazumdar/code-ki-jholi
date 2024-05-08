a4f89f6e8c26ebfb8a0cc24d7febe550462f2a54
Ayan Mazumdar <164352702+ayan-mazumdar@users.noreply.github.com>
2024-05-08 12:10:57 +0530

    objects ,types and json

---
+++ basics_02/02_objects.js
@@ -0,0 +1,64 @@
+// // singleton
+// // Object.create
+
+// // object literals
+
+// const mySym= Symbol("key1")
+
+// const JsUser ={
+//     name: "ayan",
+//     "full name": "ayan mazumdar",
+//     [mySym]:"mykey1",
+//     age: 20 ,
+// //     location: "durgapur",
+//     email: "ayan@google.com",
+//     isLoggedIn: false
+// }
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+// JsUser.email= "ayan@abcd"
+// // Object.freeze(JsUser)
+// JsUser.email= "ayan@microsfot"
+
+// // console.log(JsUser);
+
+
+// JsUser.greeting= function(){
+//     console.log("Hello my frands")
+// }
+// JsUser.greeting2= function(){
+//     console.log(`Hello my frands,${this.name}`)
+// }
+// console.log(JsUser.greeting());
+// console.log(JsUser.greeting2());
+
+
+// ******************************************************
+
+ const course = {
+    coursename: "js in hindi",
+    price: "999",
+    instructor: "hitesh"
+ }
+
+//  course.instructor
+ const {instructor:inst} = course 
+ const {price:pr} = course // destucturing objects
+
+ console.log(inst);
+ console.log(pr);
+
+
+//  {
+//     "name": "ayan"
+//     "course": "js in hindi"
+//     "price": "free"
+//  }
+
+[
+    
+]
