9163cbd0662e58b9d5e11e2fc3535c3344c0cf1b
Ayan Mazumdar <164352702+ayan-mazumdar@users.noreply.github.com>
2024-07-11 18:02:44 +0530

    revision of objects

--- basics_02/02_objects.js
+++ basics_02/02_objects.js
@@ -1,57 +1,58 @@
 // // singleton
 // // Object.create // this is known as singelton method
 
-// // object literals
+// // // object literals
 
-const mySym= Symbol("key1")
+// const mySym= Symbol("key1")
 
-const JsUser ={
-    name: "ayan",
-    "full name": "ayan mazumdar",
-    [mySym]:"mykey1",
-    age: 20 ,
-//     location: "durgapur",
-    email: "ayan@google.com",
-    isLoggedIn: false
-}
+// const JsUser ={
+//     name: "ayan",
+//     "full name": "ayan mazumdar",
+//     [mySym]:"mykey1",
+//     age: 20 ,
+// //     location: "durgapur",
+//     email: "ayan@google.com",
+//     isLoggedIn: false
+// }
 //  accesing the value by the below method is correct but not good
 // console.log(JsUser.email);
 // // the below wrotten is a better way of accesing the value
 // console.log(JsUser["email"]);
 // console.log(JsUser["full name"]);
-// console.log(JsUser[mySym]);
+// // console.log(JsUser[mySym]);
 
-JsUser.email= "ayan@abcd"
-// Object.freeze(JsUser)
-JsUser.email= "ayan@microsfot"
+// JsUser.email= "ayan@abcd"
+// // Object.freeze(JsUser)
+// JsUser.email= "ayan@microsfot"
 
-console.log(JsUser);
+// console.log(JsUser);
 
 
-JsUser.greeting= function(){
-    console.log("Hello my frands")
-}
-JsUser.greeting2= function(){
-    console.log(`Hello my frands,this is ${this.name}`)
-}
-console.log(JsUser.greeting());
-console.log(JsUser.greeting2());
+// JsUser.greeting= function(){
+//     console.log("Hello my frands")
+// }
+// JsUser.greeting2= function(){
+//     console.log(`Hello my frands,this is ${this.name}`)
+// }
+// console.log(JsUser.greeting());
+// console.log(JsUser.greeting2());
 
 
 // ******************************************************
 
-//  const course = {
-//     coursename: "js in hindi",
-//     price: "999",
-//     instructor: "hitesh"
-//  }
+ const course = 
+ {
+    coursename: "js in hindi",
+    price: "999",
+    instructor: "hitesh"
+ }
 
 // //  course.instructor
-//  const {instructor:inst} = course 
-//  const {price:pr} = course // destucturing objects
+ const {instructor:inst} = course 
+ const {price:pr} = course // destucturing objects
 
-//  console.log(inst);
-//  console.log(pr);
+ console.log(inst);
+ console.log(pr);
 
 
 // //  {
