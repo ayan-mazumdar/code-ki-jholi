b257ed9f08429449ba6f4abbe01cee10f035fafb
Ayan Mazumdar <164352702+ayan-mazumdar@users.noreply.github.com>
2024-08-02 12:34:40 +0530

    IIFE

---
+++ basics_03/05_Immediatly_invoked_function_expression.js
@@ -0,0 +1,15 @@
+// immediatly Invoked Function Expression IIFE
+
+
+(function chai (){
+
+    console.log('DB CONNECTED');
+    
+})()
+// chai () // database is conected here
+// when we do not want our function to be polluted by global scope we use IIFE 
+
+// IIFE SYNTAX
+// ()() first parenthesis contains the function definition and the 2nd one is the execution 
+
+()
