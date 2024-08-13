ced9366766574af68277873f2b20f40b5d52ff96
Ayan Mazumdar <164352702+ayan-mazumdar@users.noreply.github.com>
2024-08-13 08:19:42 +0530

    switch case

---
+++ 04_advanced/switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch(month){
+    case 1:
+        console.log("Jan");
+        break
+    case 2:
+        console.log("Feb");
+        break
+    case 3:
+        console.log("march");
+        break
+    case 4:
+        console.log("april");
+        break
+    
+    default:
+        console.log("default case match");
+        break;
+        
+        
+    
+}
