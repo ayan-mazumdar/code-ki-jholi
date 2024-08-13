2816d980f29dcbe093b598428a849308f6e1d64a
Ayan Mazumdar <164352702+ayan-mazumdar@users.noreply.github.com>
2024-08-13 08:19:55 +0530

    trythy and falsy values

---
+++ 04_advanced/truthy.js
@@ -0,0 +1,23 @@
+// const userEmail = "abcd@g.com"
+const userEmail = []
+
+// if(userEmail){
+//     console.log("got user email");
+// } else{
+//     console.log("no user email");
+    
+// }
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// Truhy values
+
+// "0", 'false', " ", []. {}. function(){}, and rest all values are truthy
+
+
+if (userEmail.length === 0 ){
+    console.log("array  empty");
+    
+}
