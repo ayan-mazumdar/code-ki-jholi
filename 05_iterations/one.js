3ad80a474492542f46deca41a0127fc1d5aa3e33
Ayan Mazumdar <164352702+ayan-mazumdar@users.noreply.github.com>
2024-08-16 07:55:42 +0530

    for loop , break and continue

---
+++ 05_iterations/one.js
@@ -0,0 +1,21 @@
+// for
+
+for(let i = 0;i <= 10;i++){
+    const element = i;
+    // if(element == 5){
+    //     console.log("5 is the goal");
+        
+    // }
+    // console.log(element);
+    
+}
+
+for (let i = 0;i <= 10;i++) {
+    console.log((`outher loop: ${i}`));
+    for(let j = 0; j <= 10; j++){
+        // console.log(`inner loop ${j} and outer loop ${i} `);
+        console.log(i + '*' + j + ' = ' + i*j );
+        
+    }
+}
+
